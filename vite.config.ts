6d9f09bc774fbe208d38a92357fcc68af13bd775
yiru jang <z5480998@ad.unsw.edu.au>
2026-03-19 17:05:19 +1100

    Fixed depolyment issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.JPEG', '**/*.JPG', '**/*.png', '**/*.svg', '**/*.gif', '**/*.webp'],
 });
